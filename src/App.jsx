9684876e88904120e948751124fffbeca6d3fd82
tarequemusa <tareque.web@gmail.com>
2025-11-19 16:53:02 +0600

    Header Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-4xl text-amber-600">Cine Rental</h1>
+      <Header />
     </>
   );
 }
