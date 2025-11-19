2a31cfca054ef720078fb916a9f68670b599cd6c
tarequemusa <tareque.web@gmail.com>
2025-11-19 22:02:19 +0600

    Movie Modal Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import MovieList from "./cine/MovieList";
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList />
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
